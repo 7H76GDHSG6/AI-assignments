f7aa817e35906f3737924f895bd7d8022c97f9f8
resz2 <aryan19459@iiitd.ac.in>
2021-09-16 18:32:34 +0530

    printing code done

--- A1/main.pl
+++ A1/main.pl
@@ -4,16 +4,24 @@
 field_list([soft_dev, finance, sciences, design, hardware]).
 
 start:-
+    undo,
     intro,
-    getCourses,
-    undo.
+    stream,
+    getCourses(L),
+    courseRecs(L).
+
 
 intro:-
     write('Welcome to IIITD B.Tech elective advisory system, follow the program step by step to get your recommendations'), nl.
 
-getCourses:-
+
+stream:-
     nl, write('Please enter your stream: '), nl,
     read(Stream),
+    assert(yes(Stream)).
+
+
+getCourses(L):-
     nl, write('Enter the courses you have taken till now(Ex - [cse101, com101]): '), nl,
     read(CoursesTaken),
     nl, write('Which fields are you interested in? (y or n): '), nl,
@@ -23,7 +31,7 @@ getCourses:-
     /*
     Suggest electives to be taken
     */
-    advise(Stream, CoursesTaken).
+    advise(CoursesTaken, L).
 
 fieldCheck([]).
 fieldCheck([H|T]) :- process(H), fieldCheck(T).
@@ -34,15 +42,36 @@ process(A):-
     ((V==y)->   assert(yes(A));
     assert(no(A))).
 
-advise(S, L):-
-    write('test2').
+/*
+Finding recommendations
+*/
+advise(CoursesTaken, L):-
+    nl, write('The recommended course for you are'), nl, nl,
+    findall(X, check(X, CoursesTaken), L).
+
+check(X, CoursesTaken):-
+    course(_, X, _, _).
+
+/*
+Printing the recommended courses
+*/
+courseRecs([]).
+courseRecs([H|T]):-
+    write(H), nl, courseRecs(T).
+
 
 /*
 Elective Courses list
+course(C, N, P, T) is true for course C with name N, prerequisites P, and tags T
 */
+
+/*
+Intro courses (first 2 sems)
+*/
+course(cse101, 'Introduction to Programming', [], ['common', 'programming']).
+course(cse102, 'Data Structures & Algorithms', [cse101], ['common', 'programming']).
 
 
 undo :- retract(yes(_)), fail. 
 undo :- retract(no(_)), fail.
-undo :- retract(course(_, _, _, _, _)), fail.
 undo.
