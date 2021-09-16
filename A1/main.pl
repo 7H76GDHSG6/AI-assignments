c42dfa1609a65546993ccba838eabcdd573a85ae
resz2 <aryan19459@iiitd.ac.in>
2021-09-16 19:33:21 +0530

    pre req checking works

--- A1/main.pl
+++ A1/main.pl
@@ -7,7 +7,7 @@ start:-
     undo,
     intro,
     stream,
-    getCourses(L),
+    recommend(L),
     courseRecs(L).
 
 
@@ -21,9 +21,9 @@ stream:-
     assert(yes(Stream)).
 
 
-getCourses(L):-
-    nl, write('Enter the courses you have taken till now(Ex - [cse101, com101]): '), nl,
-    read(CoursesTaken),
+recommend(L):-
+    nl, write('Enter the courses you have taken till now(Enter end to stop): '), nl,
+    getCourses(CoursesTaken),
     nl, write('Which fields are you interested in? (y or n): '), nl,
     field_list(Fields),
     fieldCheck(Fields),
@@ -32,6 +32,11 @@ getCourses(L):-
     Suggest electives to be taken
     */
     advise(CoursesTaken, L).
+
+getCourses(X):-  read(A), stop(A, X-[]).
+stop(end, X-X):- !.
+stop(A, [A|X]-Y):- read(B), stop(B, X-Y).
+
 
 fieldCheck([]).
 fieldCheck([H|T]) :- process(H), fieldCheck(T).
@@ -50,7 +55,8 @@ advise(CoursesTaken, L):-
     findall(X, check(X, CoursesTaken), L).
 
 check(X, CoursesTaken):-
-    course(_, X, _, _).
+    % Code for checking predicates is above the courses
+    check_pre_req(CoursesTaken, X).
 
 /*
 Printing the recommended courses
@@ -61,11 +67,21 @@ courseRecs([H|T]):-
 
 
 /*
-Elective Courses list
-course(C, N, P, T) is true for course C with name N, prerequisites P, and tags T
+Checking predicates
 */
+check_pre_req(CoursesTaken, X):-
+    course(_, X, Pre, _),
+    checkMembers(CoursesTaken, Pre).
+
+checkMembers(_,[]).
+checkMembers(L,[H|T]) :-
+	member(H,L),
+	checkMembers(L,T).
 
 /*
+Elective Courses list
+course(C, N, P, T) is true for course C with name N, prerequisites P, and tags T
+
 Intro courses (first 2 sems)
 */
 course(cse101, 'Introduction to Programming', [], ['common', 'programming']).
